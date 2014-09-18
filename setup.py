4a4042ce1dde2670d109b52fc0bc4c2403bbf794
Richard Neumann <r.neumann@homeinfo.de>
2014-09-18 16:04:20 +0200

    Added misc. files

---
+++ setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+	name='homeinfodb',
+	version='1.0',
+	author='Richard Neumann',
+	author_email='mail@richard-neumann.de',
+    install_requires=['peewee'],
+	packages=['homeinfodb',],
+	license=open('LICENSE.txt').read(),
+	description='ORM for HOMEINFO\'s CRM database',
+	long_description=open('README.txt').read(),
+)
