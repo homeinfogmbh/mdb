d60475b2c8d89669665e38ca2962ff1672e9cfa5
Richard Neumann <r.neumann@homeinfo.de>
2014-10-15 16:19:04 +0200

    fixed user name

--- homeinfodb/config.py
+++ homeinfodb/config.py
@@ -7,5 +7,5 @@ __date__ = '18.09.2014'
 from peewee import MySQLDatabase
 
 deferred_db = MySQLDatabase('homeinfo', host='mysql.homeinfo.de', 
-                            user='homeinfodb', 
+                            user='homeinfo', 
                             passwd='Z"XO;$2K+>XEo}jK>6-+}|U@,|E/6_&W')
