5be6bb8faaed921e1553f732abbec5d9f2f35c7e
Richard Neumann <r.neumann@homeinfo.de>
2014-10-15 16:18:26 +0200

    Fixed mysql server path

--- homeinfodb/config.py
+++ homeinfodb/config.py
@@ -6,5 +6,6 @@ __date__ = '18.09.2014'
 
 from peewee import MySQLDatabase
 
-deferred_db = MySQLDatabase('homeinfo', host='localhost', user='homeinfodb', 
+deferred_db = MySQLDatabase('homeinfo', host='mysql.homeinfo.de', 
+                            user='homeinfodb', 
                             passwd='Z"XO;$2K+>XEo}jK>6-+}|U@,|E/6_&W')
