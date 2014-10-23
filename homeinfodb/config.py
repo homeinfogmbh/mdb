01b45dbf156632566ff76f7475da016e87ebcbeb
Richard Neumann <r.neumann@homeinfo.de>
2014-10-23 18:56:56 +0200

    Modified config

--- homeinfodb/config.py
+++ homeinfodb/config.py
@@ -6,6 +6,6 @@ __date__ = '18.09.2014'
 
 from peewee import MySQLDatabase
 
-deferred_db = MySQLDatabase('homeinfo', host='mysql.homeinfo.de', 
+database = MySQLDatabase('homeinfo', host='mysql.homeinfo.de', 
                             user='homeinfo', 
                             passwd='Z"XO;$2K+>XEo}jK>6-+}|U@,|E/6_&W')
