be7728c37744171bbe14caa7e70261c638f158ea
Richard Neumann <r.neumann@homeinfo.de>
2014-10-27 09:49:59 +0100

    Fixed DB config and added schema to meta variable

--- homeinfodb/config.py
+++ homeinfodb/config.py
@@ -6,6 +6,9 @@ __date__ = '18.09.2014'
 
 from peewee import MySQLDatabase
 
-database = MySQLDatabase('homeinfo', host='mysql.homeinfo.de', 
-                            user='homeinfo', 
-                            passwd='Z"XO;$2K+>XEo}jK>6-+}|U@,|E/6_&W')
+DB = 'homeinfo'
+HOST = 'mysql.homeinfo.de'
+USER = 'homeinfo'
+PASSWD = 'Z"XO;$2K+>XEo}jK>6-+}|U@,|E/6_&W'
+
+database = MySQLDatabase(DB, host=HOST, user=USER, passwd=PASSWD)
--- homeinfodb/models/abc.py
+++ homeinfodb/models/abc.py
@@ -7,11 +7,12 @@ __date__ = '18.09.2014'
 __all__ = ['CRMModel']
 
 from peewee import Model
-from ..config import deferred_db
+from ..config import database, DB
 
 class CRMModel(Model):
     """
     Generic HOMEINFO-DB Model
     """
     class Meta:
-        database = deferred_db
+        database = database
+        schema = DB
