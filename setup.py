175b5c3059c73b9278b19d89c00f78399db02451
Richard Neumann <r.neumann@homeinfo.de>
2014-11-13 12:48:42 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,20 @@
 from setuptools import setup
 
 setup(
-	name='homeinfodb',
-	version='1.0',
-	author='Richard Neumann',
-	author_email='mail@richard-neumann.de',
-    install_requires=['peewee'],
-	packages=['homeinfodb',
-			'homeinfodb.models'],
-	license=open('LICENSE.txt').read(),
-	description='ORM for HOMEINFO\'s CRM database',
-	long_description=open('README.txt').read(),
+    name='homeinfo-crm',
+    version='1.0',
+    author='Richard Neumann',
+    author_email='mail@richard-neumann.de',
+    install_requires=['peewee',
+                      'mysqlhacks'],
+    packages=['homeinfo_crm',
+              'homeinfo_crm.models'],
+    license=open('LICENSE.txt').read(),
+    description='ORM for HOMEINFO\'s CRM database',
+    long_description=open('README.txt').read(),
 )
 
 from homeinfodb import __tables__
 for table in __tables__:
-	print('Creating table', table)
-	table.create_table(fail_silently=True)
+    print('Creating table', table)
+    table.create_table(fail_silently=True)
