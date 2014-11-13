383dbfb0fb4ffdc823df10b09597481d21e5a959
Richard Neumann <r.neumann@homeinfo.de>
2014-11-13 12:55:08 +0100

    Moved to distutils.core

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #! /usr/bin/env python3
 
-from setuptools import setup
+from distutils.core import setup
 
 setup(
     name='homeinfo',
     version='1.0',
     author='Richard Neumann',
     author_email='mail@richard-neumann.de',
-    install_requires=['peewee',
-                      'mysqlhacks'],
+    requires=['peewee',
+              'mysqlhacks'],
     packages=['homeinfo',
               'homeinfo.crm'],
     license=open('LICENSE.txt').read(),
