565418c24b6131cc5fff82e0e620f0501cd86dca
Richard Neumann <r.neumann@homeinfo.de>
2014-12-02 17:07:21 +0100

    fix

--- setup.py
+++ setup.py
@@ -7,10 +7,20 @@ setup(
     version='1.0',
     author='Richard Neumann',
     author_email='mail@richard-neumann.de',
-    requires=['peewee'],
-    packages=['homeinfo'],
+    requires=['homeinfo'],
+    package_dir={'homeinfo': ''},
+    packages=['homeinfo.crm'],
     data_files=[('/usr/local/etc', ['files/etc/homeinfo.conf'])],
     license=open('LICENSE.txt').read(),
     description='HOMEINFO ORM database root',
     long_description=open('README.txt').read(),
 )
+
+try:
+    from homeinfo.crm import __tables__
+except:
+    print('Cannot import __tables__')
+else:
+    for table in __tables__:
+        print('Creating table', table)
+        table.create_table(fail_silently=True)
