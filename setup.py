77d0f19ccc742e5d99ae36fc8649887cd4041792
Richard Neumann <r.neumann@homeinfo.de>
2014-12-10 16:19:17 +0100

    fix

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #! /usr/bin/env python3
 
 from distutils.core import setup
+from peewee import OperationalError
 
 setup(
     name='homeinfo',
     version='1.0',
     author='Richard Neumann',
     author_email='mail@richard-neumann.de',
-    requires=['homeinfo'],
+    requires=['peewee',
+              'homeinfo'],
     package_dir={'homeinfo': ''},
     packages=['homeinfo.crm'],
     data_files=[('/usr/local/etc', ['files/etc/homeinfo.conf'])],
@@ -18,8 +20,8 @@ setup(
 
 try:
     from homeinfo.crm import __tables__
-except:
-    print('Cannot import __tables__')
+except OperationalError:
+    print('WARNING: No database access - Won\'t create any tables')
 else:
     for table in __tables__:
         print('Creating table', table)
