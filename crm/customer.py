411b033547dcffb268361cd1f88c5b5bc2e3fdfa
Richard Neumann <r.neumann@homeinfo.de>
2014-12-15 11:46:19 +0100

    Added sha256name()

--- crm/customer.py
+++ crm/customer.py
@@ -4,6 +4,7 @@ Customer related models for HOMEINFO's CRM
 from .abc import CRMModel
 from .company import Company
 from peewee import ForeignKeyField
+from hashlib import sha256
 
 __author__ = 'Richard Neumann <r.neumann@homeinfo.de>'
 __date__ = '18.09.2014'
@@ -27,6 +28,11 @@ class Customer(CRMModel):
     def cid(self, cid):
         """Sets the Customer ID"""
         self.id = cid
+
+    @property
+    def sha256name(self):
+        """Returns the SHA-256 encoded CID"""
+        return str(sha256(str(self.cid).encode()).hexdigest())
 
     @property
     def name(self):
