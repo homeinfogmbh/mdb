eb3921d345a5ddb5216499260f875294f4b807e3
Richard Neumann <r.neumann@homeinfo.de>
2015-04-24 15:47:01 +0200

    Fix

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(
-    name='homeinfo',
+    name='homeinfo-crm',
     version='1.0',
     author='Richard Neumann',
     author_email='r.neumann@homeinfo.de',
     requires=['peewee',
               'homeinfo'],
     package_dir={'homeinfo': ''},
-    py_modules=['crm.py'],
+    packages=['homeinfo.crm'],
     data_files=[('/usr/local/etc', ['files/etc/crm.conf'])],
     license=open('LICENSE.txt').read(),
     description='HOMEINFO ORM database root',
