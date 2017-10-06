c95e6041563eeb1ef3f83ed30dc75fdf96e31623
Richard Neumann <r.neumann@homeinfo.de>
2017-10-06 12:46:57 +0200

    Refac.

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ from distutils.core import setup
 setup(
     name='homeinfo-crm',
     version='latest',
-    author='Richard Neumann',
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='info@homeinfo.de',
+    maintainer='Richard Neumann',
+    maintainer_email='r.neumann@homeinfo.de',
     package_dir={'homeinfo': ''},
     py_modules=['homeinfo.crm'],
-    data_files=[('/usr/bin', ['files/usr/bin/crmgr'])],
+    scripts=['files/crmgr'],
     description='HOMEINFO Customer Relationship Management ORM')
