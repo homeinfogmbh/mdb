3d04bca1e2ae62543828ae7ef1166f1889985061
Richard Neumann <mail@richard-neumann.de>
2021-01-20 21:36:29 +0100

    Refactored customer parser.

--- mdb/parsers.py
+++ mdb/parsers.py
@@ -1,6 +1,5 @@
 """ORM model parsers."""
 
-from mdb.config import LOGGER
 from mdb.orm import Customer
 
 
@@ -8,14 +7,14 @@ __all__ = ['customer']
 
 
 def customer(string: str) -> Customer:
-    """Returns the first matched Customer."""
+    """Returns the respective customer."""
 
     try:
-        first, *ambiguous = Customer.find(string)
+        match, *excess = Customer.find(string)
     except ValueError:
         raise ValueError('No such customer.') from None
 
-    for customer in ambiguous:  # pylint: disable=W0621
-        LOGGER.warning('Found ambiguous customer: %s', customer)
+    if excess:
+        raise ValueError('Ambiguous customer selection.')
 
-    return first
+    return match
