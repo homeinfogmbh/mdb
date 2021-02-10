454bef50549712889a62dfdca76e94af19a9cae7
Richard Neumann <mail@richard-neumann.de>
2021-02-10 10:36:13 +0100

    Remove redudant docstrings on magic methods

--- mdb/exceptions.py
+++ mdb/exceptions.py
@@ -7,16 +7,14 @@ __all__ = ['AlreadyExists']
 
 
 class AlreadyExists(Exception):
-    """Indicates that a certainr record already exists."""
+    """Indicates that a certain record already exists."""
 
     def __init__(self, record: Model, **keys):
-        """Sets the record and key."""
         super().__init__(record, keys)
         self.record = record
         self.keys = keys
 
     def __str__(self):
-        """Prints record and keys."""
         record_name = type(self.record).__name__
 
         if self.keys:
