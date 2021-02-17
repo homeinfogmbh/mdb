a3e73d53aaf2f8cf3b7de64cbf0d0e7370317492
Richard Neumann <mail@richard-neumann.de>
2021-02-17 11:24:44 +0100

    Update copyright notice

--- mdb.mjs
+++ mdb.mjs
@@ -1,7 +1,7 @@
 /*
     mdb.mjs - Library for MDB data structures.
 
-    (C) 2020 HOMEINFO - Digitale Informationssysteme GmbH
+    (C) 2020-2021 HOMEINFO - Digitale Informationssysteme GmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
