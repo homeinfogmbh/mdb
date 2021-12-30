b4661701d0ce3cf73b541942b764757917bcce63
Richard Neumann <r.neumann@homeinfo.de>
2021-12-30 14:32:16 +0100

    Export State and zip code stuff

--- mdb/__init__.py
+++ mdb/__init__.py
@@ -1,5 +1,6 @@
 """HOMEINFO's main database."""
 
+from mdb.enumerations import State
 from mdb.exceptions import AlreadyExists
 from mdb.orm import DATABASE
 from mdb.orm import Address
@@ -9,16 +10,22 @@ from mdb.orm import Department
 from mdb.orm import Employee
 from mdb.orm import Tenement
 from mdb.parsers import customer
+from mdb.zip_codes import RANGES, STATES, ZIP_CODES, get_state
 
 
 __all__ = [
     'DATABASE',
+    'RANGES',
+    'STATES',
+    'ZIP_CODES',
     'AlreadyExists',
     'Address',
     'Company',
+    'Customer',
     'Department',
     'Employee',
-    'Customer',
+    'State',
     'Tenement',
-    'customer'
+    'customer',
+    'get_state'
 ]
