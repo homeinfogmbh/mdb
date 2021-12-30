3c3edb5db7eab15f5121c7f561a4e79ceaf0fc66
Richard Neumann <r.neumann@homeinfo.de>
2021-12-30 14:21:47 +0100

    Remove obsolete exports

--- mdb/__init__.py
+++ mdb/__init__.py
@@ -4,11 +4,9 @@ from mdb.exceptions import AlreadyExists
 from mdb.orm import DATABASE
 from mdb.orm import Address
 from mdb.orm import Company
-from mdb.orm import Country
 from mdb.orm import Customer
 from mdb.orm import Department
 from mdb.orm import Employee
-from mdb.orm import State
 from mdb.orm import Tenement
 from mdb.parsers import customer
 
@@ -16,8 +14,6 @@ from mdb.parsers import customer
 __all__ = [
     'DATABASE',
     'AlreadyExists',
-    'Country',
-    'State',
     'Address',
     'Company',
     'Department',
